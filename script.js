23dbc473dd1b688d874e5d1f9699744eb33a1dca
ShreyaKumar-dev <shreyatiwary2003@gmail.com>
2024-07-22 02:53:37 +0530

    about us

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ var navBar = document.getElementById('navBar');
                 // navBar.classList.remove('shadow');
             }
         }
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
